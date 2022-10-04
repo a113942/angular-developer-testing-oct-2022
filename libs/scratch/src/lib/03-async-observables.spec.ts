0012c03ee9847decbc11a4f9a4b2d21aa0a0789d
Daniel Sarinana <Daniel_E_Sarinana@progressive.com>
2022-10-04 12:32:20 -0400

    lunch 10-04

--- libs/scratch/src/lib/03-async-observables.spec.ts
+++ libs/scratch/src/lib/03-async-observables.spec.ts
@@ -1,12 +1,20 @@
-import { Observable, of, tap } from 'rxjs';
+import { map, Observable, Observer, of, tap } from 'rxjs';
+
 describe('basic structure for testing a method or function that returns an observable', () => {
   it('testing the sendToServer', (done) => {
+    //let observer: Observer;
+
+    // We can't say we are done until the sendToServer returns a value.
+    // if we take the 'done' callback on the function for the "it", it won't let the test complete until we call that function.
+    // There is a configurable timeout.. I can't remember what the default is, but in a unit test, it should be plenty.
     sendToServer({ message: 'Lunch Time', priority: 'HIGH' })
-      .pipe(
-        tap((response) => expect(response).toEqual('Done!')),
-        tap(() => done())
-      )
-      .subscribe();
+      .pipe(map((response) => response.toUpperCase()))
+      .subscribe({
+        next: (val) => {
+          expect(val).toBe('DONE!');
+        },
+        complete: () => done(),
+      });
   });
 });
 
@@ -14,6 +22,6 @@ function sendToServer(log: {
   message: string;
   priority: 'HIGH' | 'MEDIUM' | 'LOW';
 }): Observable<string> {
-  // do the theing...
+  // do the thing...
   return of('Done!');
 }
