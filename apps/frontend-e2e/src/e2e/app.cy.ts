3fed74792f1c433bc9c2a0e4dce7d9521c7d8824
Daniel Sarinana <Daniel_E_Sarinana@progressive.com>
2022-10-04 15:01:12 -0400

    second break 10-04

--- apps/frontend-e2e/src/e2e/app.cy.ts
+++ apps/frontend-e2e/src/e2e/app.cy.ts
@@ -1,21 +1,13 @@
 describe('frontend', () => {
   beforeEach(() => cy.visit('/'));
 
-  describe('Routing Defaults', () => {
-    it('Updates the location', () => {
-      cy.url().should('match', /\/home$/); // at the end of the url ($), there should be a '/' (\/) followed by 'home'
-    });
-    it('Displays the Home Component', () => {
-      cy.get('[data-testid="app-home"]').should('exist');
-    });
-  });
   it('The Header Is Set', () => {
     // Custom command example, see `../support/commands.ts` file
     cy.get('[data-testid="app-mastheader-title"]').contains(
-      'Angular Developer Testing Course'
+      'Angular Developer Testing Course',
     );
     cy.get('[data-testid="app-mastehead-subtitle"]').contains(
-      'Training to be Boss Angular Developers and Testers'
+      'Training to be Boss Angular Developers and Testers',
     );
   });
 });
